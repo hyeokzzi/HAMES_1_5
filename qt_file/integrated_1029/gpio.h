22f6d2feca5b1bcb72f003a2fe32b540a591a3a4
eunnseo <eunseo.choi.d@gmail.com>
2023-10-29 11:04:56 +0900

    Rename

---
+++ qt_file/integrated_1029/gpio.h
@@ -0,0 +1,21 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+// cover open/close button
+#define OPEN_BTN_GPIO 24
+#define CLOSE_BTN_GPIO 23
+
+// PIR sensor
+#define PIR_GPIO_IN 17
+
+// ADC (rain, joystick sensor)
+#define CS_MCP3208 8 // BCM 8
+#define JOYSTICK_SW 5
+
+// ultrasonic wave sensor
+#define UWAVE_TRIG_PIN 18   // BCM
+#define UWAVE_ECHO_PIN 21   // BCM
+//#define UWAVE_TRIG_PIN 1  // wiringPi
+//#define UWAVE_ECHO_PIN 29 // wiringPi
+
+#endif // GPIO_H
---
+++ qt_file/integrated_1029/mainwindow.h
@@ -0,0 +1,68 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "mpu_sensor.h"
+#include "pir_sensor.h"
+#include "rain_sensor.h"
+#include "uwave_sensor.h"
+#include "logic.h"
+#include "button.h"
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+extern int gPir;
+extern int gUwave;
+extern int gRain;
+extern double gGyroX;
+extern double gGyroY;
+extern int gJoystick;
+extern int gOpenButton;
+extern int gCloseButton;
+
+extern int gWindowState;
+extern int gCoverState;
+extern int gOpenDegree;
+extern int gLED;
+extern int gUserMode;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+
+
+private slots:
+    void on_AutoMode_clicked();
+    void on_RefreshMode_clicked();
+    void on_LightMode_clicked();
+    void on_SelectMode_clicked();
+    void on_userMode_clicked();
+    void on_Btn_cover_clicked();
+    void on_WindowSlider_valueChanged(int value);
+
+    void on_WindowSlider_sliderReleased();
+
+private:
+    Ui::MainWindow *ui;
+    bool IsClicked1;
+    bool IsClicked2;
+    bool IsClicked3;
+
+    // thread
+    MPU_Sensor *mpu_thread;
+    PIR_Sensor *pir_thread;
+    Rain_Sensor *rain_thread;
+    UWAVE_Sensor *uwave_thread;
+    Logic *logic_thread;
+
+};
+#endif // MAINWINDOW_H
